779bc9e38c1c1ed3966b6b60f8492df93954d22d
Goel Ankit <ankit.goel@volvo.com>
2018-06-07 16:48:47 +0530

    First commit

---
+++ src/Board/board.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './board.css';
+import Square from '../Square/square';
+import calculateWinner from '../Winner/calculateWinner';
+
+class Board extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      };
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+
+    resetState() {
+      this.setState({squares:Array(9).fill(null)})
+      this.setState({xIsNext: true})
+    }
+  
+    render() {
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } 
+      else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+  
+      return (
+        <div>
+          <div className="status">
+            <span className="statusMsg">{status}</span>
+            <button className="restart" onClick={() => this.resetState()}>Restart the Game</button>
+          </div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default Board;
